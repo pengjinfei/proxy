8ca03598c764b341cb57615a0e44dae60b8ff432
pengjinfei <pengjinfei@126.com>
2018-03-17 18:18:54 +0800

    AesUtils sneaky throws

--- proxycommon/src/main/java/com/pengjinfei/proxy/util/AesUtils.java
+++ proxycommon/src/main/java/com/pengjinfei/proxy/util/AesUtils.java
@@ -9,7 +9,6 @@ import org.springframework.util.Assert;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidKeyException;
 
 /**
  * Created on 3/17/18
@@ -26,33 +25,23 @@ public class AesUtils {
      */
     private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
 
-    public static byte[] encrypt(byte[] content, byte[] password) throws InvalidKeyException {
-        try {
-            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(password));
-            return cipher.doFinal(content);
-        } catch (InvalidKeyException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    @SneakyThrows
+    public static byte[] encrypt(byte[] content, byte[] password) {
+        Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(password));
+        return cipher.doFinal(content);
     }
 
-    public static byte[] decrypt(byte[] content, byte[] password) throws InvalidKeyException {
-        try {
-            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, getSecretKey(password));
-            return cipher.doFinal(content);
-        } catch (InvalidKeyException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    @SneakyThrows
+    public static byte[] decrypt(byte[] content, byte[] password) {
+        Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, getSecretKey(password));
+        return cipher.doFinal(content);
     }
 
     @SneakyThrows
     private static SecretKey getSecretKey(@NonNull byte[] bytes) {
-        Assert.state(bytes.length==16,"SecretKey must be 16 length.");
-        return new SecretKeySpec(bytes,KEY_ALGORITHM);
+        Assert.state(bytes.length == 16, "SecretKey must be 16 length.");
+        return new SecretKeySpec(bytes, KEY_ALGORITHM);
     }
 }
--- proxycommon/src/test/java/com/pengjinfei/proxy/util/AesUtilsTest.java
+++ proxycommon/src/test/java/com/pengjinfei/proxy/util/AesUtilsTest.java
@@ -4,14 +4,12 @@ import org.apache.commons.lang3.RandomUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.security.InvalidKeyException;
-
 public class AesUtilsTest {
 
     private byte[] passwd = RandomUtils.nextBytes(16);
 
     @Test
-    public void encryptAndDecrypt() throws InvalidKeyException {
+    public void encryptAndDecrypt() {
         String origin = "pjf";
         byte[] encrypt = AesUtils.encrypt(origin.getBytes(), passwd);
         System.out.println(encrypt.length);
